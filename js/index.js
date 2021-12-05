edbef5965f043272b3b93cd1b5e5ec346cb648c6
daleystep <85768958+daleystep@users.noreply.github.com>
2021-12-04 20:40:39 -0500

    beginning of website

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$("div.login-method > img").on("click", function(){
+    $("#login-overlay").removeClass("hidden")
+    $("#login-prompt").prepend("Allow Gas Buddy to connect with " + this.id + "?")
+})
+
+$("#cancel-button").click(function(){
+    $("#login-overlay").addClass("hidden")
+})
+
+$("#zip").change(function(){
+    if ($("zip").val != null){
+        $("#zip-entry").removeClass("hidden")
+    }
+})
