4b3e1da37bea05b8094c8c7826dfba1f1126ab77
daleystep <85768958+daleystep@users.noreply.github.com>
2021-12-06 21:16:37 -0500

    more detailing

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 $("div.login-method > img").on("click", function(){
+    $("#login-prompt").html("")
     $("#login-overlay").removeClass("hidden")
     $("#login-prompt").prepend("Allow Gas Buddy to connect with " + this.id + "?")
 })
 
 $("#cancel-button").click(function(){
+    $("#login-prompt").html("")
     $("#login-overlay").addClass("hidden")
 })
 
